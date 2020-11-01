4ee298b82c6d961ec1d24383a845c53c05bec7db
yuyanmei-alice <1137910825@qq.com>
2020-11-01 11:14:58 +0800

    将get_cal中的scope修改为module

---
+++ app/test_demo.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-'
+'''
+@author : yuyanmei
+@Date :2020/11/1 9:51
+'''
+
+# This sample code uses the Appium python client
+# pip install Appium-Python-Client
+# Then you can paste this into a file and simply run with Python
+
+from appium import webdriver
+from appium.webdriver.common.mobileby import MobileBy
+
+
+class TestAppium():
+    def setup(self):
+        caps = {}
+        caps["platformName"] = "android"
+        caps["deviceName"] = "127.0.0.1:62001"
+        caps["appPackage"] = "com.tencent.wework"
+        caps["appActivity"] = ".launch.LaunchSplashActivity"
+        caps["noReset"] = "True"
+
+        self.driver = webdriver.Remote("http://localhost:4723/wd/hub", caps)
+        self.driver.implicitly_wait(5)
+
+    # 登录企业微信>点击工作台>打卡
+    def test_appium(self):
+        self.driver.find_element(MobileBy.XPATH, "//*[@text='工作台']").click()
+        self.driver.find_element(MobileBy.ANDROID_UIAUTOMATOR,
+                                 'new UiScrollable(new UiSelector()\
+                                 .scrollable(true).instance(0))\
+                                 .scrollIntoView(new UiSelector()\
+                                 .text("打卡").instance(0));').click()
+        self.driver.find_element(MobileBy.XPATH, "//*[@text='外出打卡']").click()
+        self.driver.find_element(MobileBy.XPATH, "//*[contains(@text,'次外出')]").click()
+        assert "外出打卡成功" in self.driver.page_source
+
+    def teardown(self):
+        self.driver.quit()
