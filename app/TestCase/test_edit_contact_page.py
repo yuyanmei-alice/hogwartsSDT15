d650c77c7abdf8869df9f33e52cd2ad3953c773b
yuyanmei-alice <1137910825@qq.com>
2020-11-09 17:32:02 +0800

    企业微信>编辑个人信息页面测试类 增加setup和teardown

--- app/TestCase/test_edit_contact_page.py
+++ app/TestCase/test_edit_contact_page.py
@@ -14,13 +14,19 @@ from app.Pages.main_page import MainPage
 
 
 class TestEditContactPage():
-    @pytest.mark.parametrize('name,phoneNum', [('小兰1', '15811112223')])
+    def setup_class(self):
+        # 启动应用
+        self.app = BasicPage()
+
+    def teardown_class(self):
+        self.app.stop()
+
+    # 此处逻辑待优化判断测试联系人是否已存在，如果已存在，就直接进行删除操作，如果不存在先调用添加联系人
+    @pytest.mark.parametrize('name,phoneNum', [('小兰', '15811112223')])
     def test_delete_contact(self, name, phoneNum):
-        # 先添加测试联系人
-        MainPage().goto_contactList().add_contact().goto_addMember_manual().add_member(name, phoneNum)
-        # 编辑成员>删除成员
-        MainPage().goto_contactList().goto_search_contact().search_by_name(
-            name).goto_contact_detail_page().goto_more().goto_edit_contact().\
+        self.app.goto_main().goto_contactList().goto_search_contact().search_by_name(
+            name).goto_contact_detail_page().goto_more().goto_edit_contact(). \
             delete_contact().waitElementPresent(By.XPATH, "//*[@text='无搜索结果']")
+
 
 
