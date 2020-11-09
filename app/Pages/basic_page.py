795b47956dca1505cae77fb5ae20fcca7192eb13
yuyanmei-alice <1137910825@qq.com>
2020-11-09 11:48:50 +0800

    企业微信app>基类模块

---
+++ app/Pages/basic_page.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-'
+'''
+@author : yuyanmei
+@Date :2020/11/7 16:04
+'''
+
+from appium import webdriver
+from appium.webdriver.webdriver import WebDriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class BasicPage():
+    def __init__(self, driver: WebDriver = None):
+        if driver is None:
+            caps = {}
+            caps["platformName"] = "android"
+            caps["deviceName"] = "127.0.0.1:62001"
+            caps["appPackage"] = "com.tencent.wework"
+            caps["appActivity"] = ".launch.LaunchSplashActivity"
+            caps["noReset"] = "True"
+            caps["skipDeviceInitialization"] = "True"
+            caps["skipServerInstallation"] = "True"
+            # caps["dontStopAppOnReset"] = "True"
+            self.driver = webdriver.Remote("http://localhost:4723/wd/hub", caps)
+            self.driver.implicitly_wait(5)
+        else:
+            self.driver = driver
+
+    def findAndClick(self, by, locator):
+        return self.driver.find_element(by, locator).click()
+
+    def findAndSendKeys(self, by, locator, value):
+        return self.driver.find_element(by, locator).send_keys(value)
+
+    def getPageSource(self):
+        return self.driver.page_source
+
+    def waitElementPresent(self, by, locator):
+        return WebDriverWait(self.driver, 5, 0.5).until(EC.presence_of_all_elements_located((by, locator)))
