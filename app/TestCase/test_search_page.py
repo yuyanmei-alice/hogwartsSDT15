78c3513789d664fc29dc78d1b1273af3f58162ce
yuyanmei-alice <1137910825@qq.com>
2020-11-09 17:32:45 +0800

    企业微信>通讯录搜索页面测试类 增加test_is_have_searchResult

--- app/TestCase/test_search_page.py
+++ app/TestCase/test_search_page.py
@@ -10,12 +10,25 @@ from app.Pages.main_page import MainPage
 
 
 class TestSearchPage():
+
+    def setup(self):
+        from app.Pages.basic_page import BasicPage
+        self.app = BasicPage()
+
+    def teardown(self):
+        self.app.stop()
+
     # 首页>通讯录>搜索>输入名称搜索
     @pytest.mark.parametrize('name', ['王五'])
     def test_search_by_name(self, name):
-        MainPage().goto_contactList().goto_search_contact().search_by_name(name)
+        self.app.goto_main().goto_contactList().goto_search_contact().search_by_name(name)
 
     # 首页>通讯录>搜索>输入名称搜索>选中查询结果进入个人信息页面
     @pytest.mark.parametrize('name', ['王五'])
     def test_goto_contact_detail_page(self, name):
-        MainPage().goto_contactList().goto_search_contact().search_by_name(name).goto_contact_detail_page()
+        self.app.goto_main().goto_contactList().goto_search_contact().search_by_name(name).goto_contact_detail_page()
+
+    # 首页>通讯录>搜索>输入名称搜索> 返回查询结果
+    @pytest.mark.parametrize('name', ['小兰', '无'])
+    def test_is_have_searchResult(self, name):
+        print(self.app.goto_main().goto_contactList().goto_search_contact().search_by_name(name).is_have_searchResult())
