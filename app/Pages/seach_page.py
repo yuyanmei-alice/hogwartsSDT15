69f676ecdc4963c4f42d21b2a837f825d7d9cc39
yuyanmei-alice <1137910825@qq.com>
2020-11-09 11:50:11 +0800

    企业微信app>通讯录>搜索页面模块

---
+++ app/Pages/seach_page.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-'
+'''
+@author : yuyanmei
+@Date :2020/11/7 17:28
+'''
+from selenium.webdriver.common.by import By
+
+from app.Pages.basic_page import BasicPage
+
+
+class SearchPage(BasicPage):
+
+    def search_by_name(self, name):
+        self.findAndSendKeys(By.XPATH, "//*[@text='搜索']", name)
+        return SearchPage(self.driver)
+
+    def goto_contact_detail_page(self):
+        self.findAndClick(By.XPATH, "//*[@resource-id='com.tencent.wework:id/gja']/android.widget.RelativeLayout[2]")
+        from app.Pages.contact_detail_page import ContactDetailPage
+        return ContactDetailPage(self.driver)
+
+
+
+
+
+
+
